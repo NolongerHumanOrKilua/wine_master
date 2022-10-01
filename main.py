15b12df67638e5789856594976125af0ea6c0a6f
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-10-01 13:47:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,65 @@
+from ast import arg
+from email.policy import default
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import datetime
-
+import argparse
+import os
 import pandas
 import collections
 from pprint import pprint
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html', 'xml'])
-)
-
-template = env.get_template('template.html')
-
-
-event1 = datetime.datetime(year=1920, month=1, day=1, hour=0)
-event2 = datetime.datetime.now()
-delta = event2.year-event1.year
-if 11 <= delta <= 20:
-    years = "лет"
-elif delta % 10 == 2 or delta % 10 == 3 or delta % 10 == 4:
-    years = "года"
-elif delta % 10 == 1:
-    years = "год"
-else:
-    years = "лет"
-
-excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1', na_values='nan', keep_default_na=False, usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', "Акция"])
-drinks = excel_data_df.to_dict(orient='records')
-
-dict_of_lists = collections.defaultdict(list) 
-
-for drink in drinks:
-    dict_of_lists[drink["Категория"]].append(drink)
-
-wines = dict_of_lists.items()
 
 
 
 
+def years(count_of_years):
+    if 11 <= count_of_years <= 20:
+        years = "лет"
+    elif count_of_years % 10 == 2 or count_of_years % 10 == 3 or count_of_years % 10 == 4:
+        years = "года"
+    elif count_of_years % 10 == 1:
+        years = "год"
+    else:
+        years = "лет"
+    return years     
 
-rendered_page = template.render(
-    number_of_years=years,
-    year=delta,
-    wines=wines,
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    template = env.get_template('template.html')    
+    year_of_foundation = datetime.datetime(year=1920, month=1, day=1, hour=0)
+    present_year = datetime.datetime.now()
+    count_of_years = present_year.year-year_of_foundation.year
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-file", default="wine3.xlsx")
+    parser.add_argument("-sheet_name", default="Лист1")                   
+    args = parser.parse_args()
+    excel_data_df = pandas.read_excel(args.file, sheet_name=args.sheet_name, na_values='nan', keep_default_na=False)
+    drinks = excel_data_df.to_dict(orient='records')
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+    dict_of_lists = collections.defaultdict(list) 
+
+    for drink in drinks:
+        dict_of_lists[drink["Категория"]].append(drink)    
+    wines = dict_of_lists.items()
+
+    rendered_page = template.render(
+        number_of_years=count_of_years,
+        year=years(count_of_years ),
+        wines=wines,
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+
+
+if __name__ == "__main__":
+    main()
